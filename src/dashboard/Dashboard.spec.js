6bd154842cd83a5ec9b0ada7200301417f2b1c3e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:14:28 -0400

    Creates a 'describe();' statement with two test cases for the <Dashboard /> component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,3 +3,19 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 
 import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Dashboard />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  
+    // Step 2: Write this test.
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Dashboard />); // generates a DOM tree
+  
+      // snapshots are a JSON representation of the DOM tree
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+});
