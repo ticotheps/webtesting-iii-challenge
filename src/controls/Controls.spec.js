3cfd664631d19b84baf6f95f056a38a0354950ec
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:11:07 -0400

    Creates a 'describe();' statement with a two test cases for the <Controls /> component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,3 +3,19 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 
 import Controls from './Controls';
+
+describe('<Controls />', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Controls />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  
+    // Step 2: Write this test.
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Controls />); // generates a DOM tree
+  
+      // snapshots are a JSON representation of the DOM tree
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
