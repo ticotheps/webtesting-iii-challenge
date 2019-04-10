14b452d7391cee688add06a7aeae1bc6e2d13003
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 15:12:07 -0400

    Creates two more test cases for the '<Dashboard />' test suite (inside of Dashboard.spec.js) the checks to make sure that the controls and the display are shown

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import { render, fireEvent, cleanup } from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
 
-import Dashboard from './Dashboard';
+import Dashboard from "./Dashboard";
 
 afterEach(cleanup);
 
-describe('<Dashboard />', () => {
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<Dashboard />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-  
-    it('matches snapshot', () => {
-      const tree = renderer.create(<Dashboard />);
-  
-      expect(tree.toJSON()).toMatchSnapshot();
-    });
+describe("<Dashboard />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Dashboard />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("should show the controls", () => {
+    const { getByText } = render(<Dashboard />);
+
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+  });
+
+  it("should show the display", () => {
+    const { getByText } = render(<Dashboard />);
+
+    getByText(/unlocked/i);
+    getByText(/open/i);
+  });
 });
