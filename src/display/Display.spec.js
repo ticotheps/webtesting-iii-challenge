fc702331ff558b872f9cd7fd49c5a70edc761106
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 15:06:13 -0400

    Creates a new test suite (within the <Display /> test suite) for 'the gate' that includes two new test cases

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import { render, fireEvent, cleanup } from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
 
-import Display from './Display';
+import Dashboard from "../dashboard/Dashboard";
+import Display from "./Display";
 
 afterEach(cleanup);
 
-describe('<Display />', () => {
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<Display />, div);
-      ReactDOM.unmountComponentAtNode(div);
+describe("<Display />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Display />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  describe("the gate", () => {
+    it("should default to unlocked", () => {
+      const { getByText } = render(<Dashboard />);
+
+      getByText(/unlocked/i);
     });
-  
-    it('matches snapshot', () => {
-      const tree = renderer.create(<Display />);
-  
-      expect(tree.toJSON()).toMatchSnapshot();
+
+    it("should default to open", () => {
+      const { getByText } = render(<Dashboard />);
+
+      getByText(/open/i);
     });
+  });
 });
