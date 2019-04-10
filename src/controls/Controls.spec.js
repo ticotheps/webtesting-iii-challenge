440cbec175383b038116eb4620cee9059bdb8b56
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:08:31 -0400

    Imports all necessary dependencies and files into each .spec.js test file

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,5 @@
-// Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,5 @@
-// Test away
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
